49fdb1ca7a30acbd800fa312dcebcf2700dc6c01
mls42 <laspina92@gmail.com>
2024-12-22 11:21:39 +0100

    Add Recipe List

---
+++ resources/js/Pages/Recipes.vue
@@ -0,0 +1,39 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Recipe from '../Components/Recipe.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Recipes" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2>Recipes</h2>
+        </template>
+
+        <div class="content-wrapper">
+            <ul class="recipe-list">
+                <!-- @foreach -->
+                <li>
+                    <Recipe
+                        title="Spaghetti Carbonara"
+                        :ingredients="['Spaghetti', 'Eggs', 'Parmesan', 'Bacon', 'Black Pepper']"
+                        preparation="Boil spaghetti. Cook bacon. Mix eggs with parmesan and combine with cooked spaghetti and bacon."
+                        difficulty="Medium"
+                        :time="30"
+                    />
+                </li>
+                <!-- @endforeach -->
+            </ul>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script>
+    export default {
+        components: {
+            Recipe,
+        },
+    };
+</script>
